fd3f58d25dd189eb6a0b16f740e23d0438556e35
foreverfl <forever_fl@naver.com>
2022-09-23 04:55:06 +0900

    first commit

---
+++ src/main/java/com/example/TrekkingMap/TrekkingMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrekkingMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrekkingMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrekkingMapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrekkingMap/TrekkingMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrekkingMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrekkingMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
